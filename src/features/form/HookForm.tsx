61da3d6a0e2f52bc4a6e705a6e4f97adae9520c3
Kuitunen Sara <Sara.Kuitunen@syke.fi>
2023-09-08 11:28:55 +0300

    Have onSubmit function, instead of arroẃ function

--- src/features/form/HookForm.tsx
+++ src/features/form/HookForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   FormErrorMessage,
   FormLabel,
@@ -7,15 +7,20 @@ import {
   Button,
 } from "@chakra-ui/react";
 
+type Inputs = {
+  name: string;
+};
+
 export default function HookForm() {
-  const { handleSubmit, register } = useForm();
+  // UseForm needs to have same type as used in onSubmit function's data
+  // so that handleSubmit knows about the type of passed onSubmit's data
+  // https://stackoverflow.com/questions/71275687/type-of-handlesubmit-parameter-in-react-hook-form
+  const { handleSubmit, register } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        console.log(data);
-      })}
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <FormLabel htmlFor="name">First name</FormLabel>
         <Input id="name" placeholder="name" {...register("name")} />
