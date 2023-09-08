68a8380d3055c2730c08a3c009411e72c42c0562
Kuitunen Sara <Sara.Kuitunen@syke.fi>
2023-09-08 14:06:47 +0300

    Remove logos and update heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import HookForm from "./features/form/HookForm";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import "./App.css";
@@ -7,17 +5,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="pt-1 pb-1">Vite + React + Chakra UI + Zod</h1>
       <ChakraProvider>
-        <Box p={4}>
+        <Box p={2}>
           <HookForm />
         </Box>
       </ChakraProvider>
