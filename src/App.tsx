4c3c234a87bb35027d4cd935a3f34ebaabdffe87
Kuitunen Sara <Sara.Kuitunen@syke.fi>
2023-09-08 17:03:55 +0300

    Make second form using directly example from Chakra UI doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import HookForm from "./features/form/HookForm";
+import HookFormChakraEx from "./features/form/HookFormChakraEx";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import "./App.css";
 
@@ -9,6 +10,16 @@ function App() {
       <ChakraProvider>
         <Box p={2}>
           <HookForm />
+        </Box>
+        <Box p={2}>
+          <p className="pt-1">
+            Example from{" "}
+            <a href="https://chakra-ui.com/getting-started/with-hook-form">
+              Chakra documentation
+            </a>{" "}
+            which has issues because is actually JavaScript.
+          </p>
+          <HookFormChakraEx />
         </Box>
       </ChakraProvider>
     </>
