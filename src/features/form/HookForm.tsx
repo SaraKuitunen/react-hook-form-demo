9a156c7c3e6d71486608eb1af2e4a8a94d3e4db1
Kuitunen Sara <Sara.Kuitunen@syke.fi>
2023-09-08 13:13:49 +0300

    Add user input validation using zod

--- src/features/form/HookForm.tsx
+++ src/features/form/HookForm.tsx
@@ -6,16 +6,27 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 type Inputs = {
   name: string;
 };
 
+// creating a schema for strings
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+});
+
 export default function HookForm() {
   // UseForm needs to have same type as used in onSubmit function's data
   // so that handleSubmit knows about the type of passed onSubmit's data
   // https://stackoverflow.com/questions/71275687/type-of-handlesubmit-parameter-in-react-hook-form
-  const { handleSubmit, register } = useForm<Inputs>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: zodResolver(schema) });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
@@ -24,6 +35,7 @@ export default function HookForm() {
       <FormControl>
         <FormLabel htmlFor="name">First name</FormLabel>
         <Input id="name" placeholder="name" {...register("name")} />
+        {errors.name?.message && <p>{errors.name?.message}</p>}
         <FormErrorMessage>
           <p> error placeholder</p>
         </FormErrorMessage>
